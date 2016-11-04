d57f374bfdeb82821da8bc2ad487c6fd4649976e
Satchel Grant <grantsrb@gmail.com>
2016-11-04 08:27:38 -0700

    add dynamic route for groups to be displayed. Also added blank display component

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.findAll('group');
+  },
+  actions: {
+    reRoute(group) {
+      this.transitionTo('group-page', group.id);
+    }
   }
 });
