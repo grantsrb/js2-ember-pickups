7cbf8efdfeb30f00ea28a74f6b4af60d71576d52
Satchel Grant <grantsrb@gmail.com>
2016-11-04 01:24:57 -0700

    add group display capabilities. Also adds minor styling

--- app/components/group-form.js
+++ app/components/group-form.js
@@ -13,12 +13,12 @@ export default Ember.Component.extend({
     makeGroup() {
       var params = {
         sport: this.get('sportChoice'),
-        location: this.get('location'),
-        day: this.get('day'),
-        time: this.get('time'),
+        location: this.get('location') ? this.get('location'):'',
+        day: this.get('day') ? this.get('day'):'',
+        time: this.get('time') ? this.get('time'):'',
         skill: this.get('skillChoice'),
-        cost: this.get('cost'),
-        details: this.get('details')
+        cost: this.get('cost') ? this.get('cost'):'',
+        details: this.get('details') ? this.get('details'):''
       };
       this.set('sportChoice', 'Soccer');
       this.set('skillChoice', "All Skill Levels");
