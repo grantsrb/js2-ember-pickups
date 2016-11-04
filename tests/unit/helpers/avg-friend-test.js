be6ab06dbd83b6ab7a97a8c9b6408dc6c0f78e3d
Satchel Grant <grantsrb@gmail.com>
2016-11-04 15:28:07 -0700

    add minor styling

---
+++ tests/unit/helpers/avg-friend-test.js
@@ -0,0 +1,12 @@
+
+import { avgFriend } from 'js2-ember-pickup-reviews/helpers/avg-friend';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | avg friend');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = avgFriend([42]);
+  assert.ok(result);
+});
+
---
+++ tests/unit/helpers/avg-skill-test.js
@@ -0,0 +1,12 @@
+
+import { avgSkill } from 'js2-ember-pickup-reviews/helpers/avg-skill';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | avg skill');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = avgSkill([42]);
+  assert.ok(result);
+});
+
